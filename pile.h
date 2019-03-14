ef0a7f7fc29f5687d6301d8f83dce851c606db8f
valeclere <48153731+valeclere@users.noreply.github.com>
2019-03-14 18:19:23 +0100

    module complet et commenté

--- pile.h
+++ pile.h
@@ -3,27 +3,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h> //pour avoir le type bool
+#include <stdbool.h> /* pour avoir le type bool */
 
-typedef struct Mastruct_s{
-	int val;
-}Mastruct_t;
 
+typedef int element_t; /* type de variables présentes dans le tableau, ici on considère des int */
 
 typedef struct Pile_s{
 	int taille;
-	Mastruct_t nbelt;
-	Mastruct_t *deb;	
+	int nb_elt;
+	element_t * pt_tab;
 }Pile_t;
 
-Pile_t* initPile(int taille);
+Pile_t * initPile(int taille);
+void empiler(Pile_t * pt_pile, element_t element);
+element_t depiler(Pile_t * pt_pile);
+bool estVide(Pile_t * pt_pile); /* attention suppr l'affichage pour rapport */
+void libererPile(Pile_t * pt_pile);
 
-/*
-estVide
-empiler
-depiler
-initPile
-libererPile
-*/
+void affichage_pile_int(Pile_t * pt_pile); /* afficher pour tester, à supprimer pour rendre le projet */
+
 
 #endif
